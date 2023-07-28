9d120c53bc2269bdab01e10cd7ce3135fa434e17
verillyh <verillytmt@gmail.com>
2023-07-28 22:52:48 +0800

    Validator page done ----- Project Finished -----

--- main.js
+++ main.js
@@ -42,14 +42,43 @@ function copyToClipboard(element) {
     alert("Password copied to clipboard!")
 }
 
+function validatePassword() {
+    let password = document.getElementById("password").value
+    let passwordLength = password.length
+
+    if (passwordLength < 6) { 
+        errorMessage("Password is too short!") 
+        return
+    }
+    else if (password.search(/[A-Z]/) == -1) { 
+        errorMessage("Password needs to contain uppercase alphabet") 
+        return
+    }
+    else if (password.search(/[0-9]/) == -1) { 
+        errorMessage("Password needs to contain numbers") 
+        return
+    }
+    else if (password.search(/[!@#\4%\^&\*\(\)\[\]\{\}\\\|'";:\/\>\<,\.]/) == -1) { 
+        errorMessage("Password needs to include symbols") 
+        return
+    }
+    
+    alert("Your password is good to go!")
+}
+
+function errorMessage(message) { alert(message) }
+
 
 // When content is fully loaded...
 document.addEventListener("DOMContentLoaded", () => {
+    buttonContainer = document.getElementById("u-container-button")
     generatorButton = document.getElementById("generatorButton")
     generatorMainSection = document.getElementById("generator")
-    buttonContainer = document.getElementById("u-container-button")
-    submitGenerator = document.getElementById("submitGenerator")
     generatorOutput = document.getElementById("generator__output")
+    submitGenerator = document.getElementById("submitGenerator")
+    validatorButton = document.getElementById("validatorButton")
+    validatorMainSection = document.getElementById("validator")
+    submitValidator = document.getElementById("submitValidator")
 
     generatorButton.onclick = () => {
         toggleHide(buttonContainer);
@@ -57,4 +86,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     submitGenerator.onclick = generatePassword
     generatorOutput.onclick = () => { copyToClipboard(generatorOutput) }
+    validatorButton.onclick = () => {
+        toggleHide(buttonContainer)
+        toggleHide(validatorMainSection)
+    }
+    submitValidator.onclick = validatePassword
 })
